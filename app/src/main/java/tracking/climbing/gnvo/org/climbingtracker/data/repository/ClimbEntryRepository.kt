bd00d748744f0331fbd8d907d22fee636f5873ae
Nicolas Velasquez <gnvo.gnvo@gmail.com>
2018-11-11 20:39:18 +0200

    Each Dao needs it's Repository

--- app/src/main/java/tracking/climbing/gnvo/org/climbingtracker/data/repository/ClimbEntryRepository.kt
+++ app/src/main/java/tracking/climbing/gnvo/org/climbingtracker/data/repository/ClimbEntryRepository.kt
@@ -9,13 +9,13 @@ import java.util.concurrent.Executor
 
 class ClimbEntryRepository(application: Application, private val executor: Executor) {
     private val db: AppDatabase? = AppDatabase.getInstance(application = application)
-    private val attemptOutcomeDao: AttemptOutcomeDao? = db?.attemptOutcomeDao()
+//    private val attemptOutcomeDao: AttemptOutcomeDao? = db?.attemptOutcomeDao()
     private val climbEntryDao: ClimbEntryDao? = db?.climbEntryDao()
-    private val climbingStyleDao: ClimbingStyleDao? = db?.climbingStyleDao()
-    private val pitchDao: PitchDao? = db?.pitchDao()
-    private val routeGradeDao: RouteGradeDao? = db?.routeGradeDao()
-    private val routeStyleDao: RouteStyleDao? = db?.routeStyleDao()
-    private val routeTypeDao: RouteTypeDao? = db?.routeTypeDao()
+//    private val climbingStyleDao: ClimbingStyleDao? = db?.climbingStyleDao()
+//    private val pitchDao: PitchDao? = db?.pitchDao()
+//    private val routeGradeDao: RouteGradeDao? = db?.routeGradeDao()
+//    private val routeStyleDao: RouteStyleDao? = db?.routeStyleDao()
+//    private val routeTypeDao: RouteTypeDao? = db?.routeTypeDao()
     private val allClimbingEntries: LiveData<List<ClimbEntry>> = climbEntryDao?.getAllClimbingEntries()!!
 
     fun insert(climbEntry: ClimbEntry) {
