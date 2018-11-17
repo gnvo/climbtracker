67e28cc1e6b64e55866501c6a2b0e4be770f3d73
Nicolas Velasquez <gnvo.gnvo@gmail.com>
2018-11-17 14:27:51 +0200

    Make sure to report errors on duplicated climbentries insert

--- app/src/main/java/org/gnvo/climbing/tracking/climbingtracker/data/room/dao/ClimbEntryDao.kt
+++ app/src/main/java/org/gnvo/climbing/tracking/climbingtracker/data/room/dao/ClimbEntryDao.kt
@@ -2,15 +2,14 @@ package org.gnvo.climbing.tracking.climbingtracker.data.room.dao
 
 import android.arch.lifecycle.LiveData
 import android.arch.persistence.room.*
-import android.support.design.R
 import org.gnvo.climbing.tracking.climbingtracker.data.room.pojo.ClimbEntry
 
 @Dao
 interface ClimbEntryDao {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Insert
     fun insert(climbEntry: ClimbEntry)
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Insert
     fun init(climbEntry: List<ClimbEntry>)
 
     @Update
