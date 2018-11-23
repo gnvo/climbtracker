451924e9fbab450859d006284b9cf32e538121b3
Nicolas Velasquez <gnvo.gnvo@gmail.com>
2018-11-23 08:08:24 +0200

    Use buttons with dialogs for required fields

--- app/src/main/java/org/gnvo/climbing/tracking/climbingtracker/ui/addeditentry/AddEditEntryActivity.kt
+++ app/src/main/java/org/gnvo/climbing/tracking/climbingtracker/ui/addeditentry/AddEditEntryActivity.kt
@@ -4,19 +4,18 @@ import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.text.Editable
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.RadioButton
+import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_add_update_climb_entry.*
 import org.gnvo.climbing.tracking.climbingtracker.R
 import org.gnvo.climbing.tracking.climbingtracker.data.room.pojo.Attempt
 import org.gnvo.climbing.tracking.climbingtracker.data.room.pojo.Location
-import org.gnvo.climbing.tracking.climbingtracker.data.room.pojo.RouteGrade
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
@@ -31,6 +30,7 @@ class AddEditEntryActivity : AppCompatActivity() {
     private lateinit var viewModel: AddEditViewModel
     private var formatterDate = DateTimeFormatter.ofPattern("EEEE, d MMM yyyy")
     private var formatterTime = DateTimeFormatter.ofPattern("HH:mm")
+    private var gradesMapper: Map<String, Long>? = null
 
     private var climbEntryIdFromIntentExtra: Long = INVALID_ID
 
@@ -42,31 +42,40 @@ class AddEditEntryActivity : AppCompatActivity() {
 
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_close)
         if (intent.hasExtra(EXTRA_ID)) {
-            title = getString(R.string.edit_climb_entry)
+            title = getString(R.string.edit_attempt)
             climbEntryIdFromIntentExtra = intent.getLongExtra(EXTRA_ID, INVALID_ID)
             populateClimbEntryData()
         } else {
-            title = getString(R.string.add_climb_entry)
+            title = getString(R.string.add_attempt)
             val now = LocalDateTime.now()
             button_date.text = now.format(formatterDate)
             button_time.text = now.format(formatterTime)
         }
         setDateTimeDialogs()
-        setRouteGradesSpinner()
+        setRouteGradesDialog()
+        setDialog(button_route_type, R.array.route_type, "Select a route type")
+        setDialog(button_climb_style, R.array.climb_styles, "Select a climb style")
+        setDialog(button_outcome, R.array.outcome, "Select the attempt outcome")
     }
 
-    private fun setRouteGradesSpinner() {
-        viewModel.getAllRouteGrades().observe(this, Observer {
-            ArrayAdapter(
-                this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, it
-            ).also { adapter ->
-                // Specify the layout to use when the list of choices appears
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                // Apply the adapter to the spinner
-                spinner_grade.adapter = adapter
+    private fun setRouteGradesDialog() {
+        viewModel.getAllRouteGrades().observe(this, Observer { list ->
+            val array = list?.map { it.french as CharSequence }?.toTypedArray()
+            gradesMapper = list?.map{it.french!! to it.routeGradeId!!}?.toMap()
+            button_grade.setOnClickListener {
+                AlertDialog.Builder(this).setTitle("Pick a route grade").setItems(array) { _, which ->
+                    button_grade.text = list?.get(which).toString()
+                }.create().show()
             }
         })
+    }
+
+    private fun setDialog(button: Button, arrayId: Int, title: String) {
+        button.setOnClickListener {
+            AlertDialog.Builder(this).setTitle(title).setItems(arrayId) { dialog, which ->
+                button.text = resources.getStringArray(arrayId)[which]
+            }.create().show()
+        }
     }
 
     private fun setDateTimeDialogs() {
@@ -109,23 +118,23 @@ class AddEditEntryActivity : AppCompatActivity() {
 //
 //
 //                when (climbEntryFull.routeType) {
-//                    getString(R.string.sport) -> radio_group_route_type.check(radio_button_sport.id)
-//                    getString(R.string.trad) -> radio_group_route_type.check(radio_button_trad.id)
-//                    getString(R.string.indoors) -> radio_group_route_type.check(radio_button_indoors.id)
+//                    getString(R.string.sport) -> button_route_type.check(radio_button_sport.id)
+//                    getString(R.string.trad) -> button_route_type.check(radio_button_trad.id)
+//                    getString(R.string.indoors) -> button_route_type.check(radio_button_indoors.id)
 //                }
 //
 //                when (climbEntryFull.pitchesFull?.get(0)?.climbingStyle) {
-//                    getString(R.string.lead) -> radio_group_climbing_styles.check(radio_button_lead.id)
-//                    getString(R.string.follow) -> radio_group_climbing_styles.check(radio_button_follow.id)
-//                    getString(R.string.top_rope) -> radio_group_climbing_styles.check(radio_button_top_rope.id)
-//                    getString(R.string.solo) -> radio_group_climbing_styles.check(radio_button_solo.id)
+//                    getString(R.string.lead) -> button_climbing_styles.check(radio_button_lead.id)
+//                    getString(R.string.follow) -> button_climbing_styles.check(radio_button_follow.id)
+//                    getString(R.string.top_rope) -> button_climbing_styles.check(radio_button_top_rope.id)
+//                    getString(R.string.solo) -> button_climbing_styles.check(radio_button_solo.id)
 //                }
 //
 //                when (climbEntryFull.pitchesFull?.get(0)?.attemptOutcome) {
-//                    getString(R.string.onsight) -> radio_group_attempt_outcome.check(radio_button_onsight.id)
-//                    getString(R.string.flash) -> radio_group_attempt_outcome.check(radio_button_flash.id)
-//                    getString(R.string.redpoint) -> radio_group_attempt_outcome.check(radio_button_redpoint.id)
-//                    getString(R.string.fell_hung) -> radio_group_attempt_outcome.check(radio_button_fell_hung.id)
+//                    getString(R.string.onsight) -> button_attempt_outcome.check(radio_button_onsight.id)
+//                    getString(R.string.flash) -> button_attempt_outcome.check(radio_button_flash.id)
+//                    getString(R.string.redpoint) -> button_attempt_outcome.check(radio_button_redpoint.id)
+//                    getString(R.string.fell_hung) -> button_attempt_outcome.check(radio_button_fell_hung.id)
 //                }
 //
 //                edit_text_route_name.setText(climbEntryFull.name)
@@ -160,52 +169,40 @@ class AddEditEntryActivity : AppCompatActivity() {
     }
 
     private fun generateClimbEntryWithPitchesObject(): Attempt? {
-        if (radio_group_route_type.checkedRadioButtonId == -1) {
-            Toast.makeText(this, "Select a route type. Eg. Sport", Toast.LENGTH_LONG).show()
-            return null
-        }
-        if (radio_group_climbing_styles.checkedRadioButtonId == -1) {
-            Toast.makeText(this, "Select a climb stile. Eg. Lead", Toast.LENGTH_LONG).show()
-            return null
-        }
-        if (radio_group_attempt_outcome.checkedRadioButtonId == -1) {
-            Toast.makeText(this, "Select a attempt outcome. Eg. Onsight", Toast.LENGTH_LONG).show()
-            return null
-        }
-
         val date = LocalDate.parse(button_date.text, formatterDate)
         val time = LocalTime.parse(button_time.text, formatterTime)
+        val datetime = date.atTime(time)
 
-        val selectedRouteGrade = spinner_grade.selectedItem as RouteGrade
+        val routeType = button_route_type.text.toString()
+        val climbStyle= button_climb_style.text.toString()
+        val routeGrade = gradesMapper?.get(button_grade.text.toString()) ?: -1
+        val outcome = button_outcome.text.toString()
 
         val attempt = Attempt(
-            climbingStyle = (findViewById<View>(radio_group_climbing_styles.checkedRadioButtonId) as RadioButton).text.toString(),
-            datetime = date.atTime(time),
-            outcome = (findViewById<View>(radio_group_attempt_outcome.checkedRadioButtonId) as RadioButton).text.toString(),
-            routeGrade = selectedRouteGrade.routeGradeId!!,
+            datetime = datetime,
+            routeType = routeType,
+            climbingStyle = climbStyle,
+            routeGrade = routeGrade,
+            outcome = outcome,
             location = Location()
         )
 
-        edit_text_route_name.text.let {
-            if (!it!!.isEmpty()) attempt.routeName = it.toString()
-        }
-        edit_text_area.text.let {
-            if (!it!!.isEmpty()) attempt.location!!.area = it.toString()
-        }
-        edit_text_sector.text.let {
-            if (!it!!.isEmpty()) attempt.location!!.sector = it.toString()
-        }
-        edit_text_comment.text.let {
-            if (!it!!.isEmpty()) attempt.comment = it.toString()
-        }
-
-        attempt.routeType =
-                (findViewById<View>(radio_group_route_type.checkedRadioButtonId) as RadioButton).text.toString()
+        attempt.routeName = getStringOrNull(edit_text_route_name.text)
+        attempt.location!!.area = getStringOrNull(edit_text_area.text)
+        attempt.location!!.sector = getStringOrNull(edit_text_sector.text)
+        attempt.comment = getStringOrNull(edit_text_comment.text)
 
         if (rating_bar_rating.rating > 0)
             attempt.rating = rating_bar_rating.rating.toInt()
 
         return attempt
+    }
+
+    private fun getStringOrNull(text: Editable?): String? {
+        return when {
+            text.isNullOrEmpty() -> null
+            else -> return text.toString()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
