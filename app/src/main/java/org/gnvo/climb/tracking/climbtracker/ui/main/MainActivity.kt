c77d16b87ab72b6029af951eb254e148f35c3a89
Nicolas Velasquez <gnvo.gnvo@gmail.com>
2018-12-04 11:26:51 +0100

    Auto indent

--- app/src/main/java/org/gnvo/climb/tracking/climbtracker/ui/main/MainActivity.kt
+++ app/src/main/java/org/gnvo/climb/tracking/climbtracker/ui/main/MainActivity.kt
@@ -61,19 +61,24 @@ class MainActivity : AppCompatActivity() {
             adapter.submitList(it)
         })
 
-        val simpleItemTouchCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+        val simpleItemTouchCallback =
+            object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
 
-            override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
-                return false
-            }
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+                    return false
+                }
 
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                val position = viewHolder.adapterPosition
-                val attempt = adapter.getItemAt(position).attempt
-                viewModel.deleteAttempt(attempt)
-                Toast.makeText(this@MainActivity, "Deleted attempt", Toast.LENGTH_LONG).show()
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    val position = viewHolder.adapterPosition
+                    val attempt = adapter.getItemAt(position).attempt
+                    viewModel.deleteAttempt(attempt)
+                    Toast.makeText(this@MainActivity, "Deleted attempt", Toast.LENGTH_LONG).show()
+                }
             }
-        }
         val itemTouchHelper = ItemTouchHelper(simpleItemTouchCallback)
         itemTouchHelper.attachToRecyclerView(recycler_view)
 
