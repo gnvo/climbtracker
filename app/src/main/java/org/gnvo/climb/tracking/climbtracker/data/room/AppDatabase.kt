3435f66947cededd1a10e8d5e5fc31578c9f0892
Nicolas Velasquez <gnvo.gnvo@gmail.com>
2018-12-16 09:24:38 +0100

    Add migration script from db 3 to db 4

--- app/src/main/java/org/gnvo/climb/tracking/climbtracker/data/room/AppDatabase.kt
+++ app/src/main/java/org/gnvo/climb/tracking/climbtracker/data/room/AppDatabase.kt
@@ -44,6 +44,38 @@ abstract class AppDatabase : RoomDatabase() {
                 }
             }
 
+            val MIGRATION_3_4 = object : Migration(3, 4) {
+                override fun migrate(database: SupportSQLiteDatabase) {
+                    database.execSQL("PRAGMA foreign_keys=off;")
+                    database.execSQL("BEGIN TRANSACTION;")
+                    database.execSQL("ALTER TABLE attempt RENAME TO attempt_old;")
+                    database.execSQL("CREATE TABLE `attempt` " +
+                            "(`id` INTEGER PRIMARY KEY AUTOINCREMENT," +
+                            "`climb_style` TEXT NOT NULL," +
+                            "`outcome` TEXT NOT NULL," +
+                            "`route_grade` INTEGER NOT NULL," +
+                            "`route_type` TEXT NOT NULL," +
+                            "`route_name` TEXT," +
+                            "`comment` TEXT," +
+                            "`rating` INTEGER," +
+                            "`route_characteristics` TEXT," +
+                            "`length` INTEGER," +
+                            "`location` INTEGER," +
+                            "`instant` INTEGER NOT NULL," +
+                            "`zone_id` TEXT NOT NULL," +
+                            "FOREIGN KEY(`route_grade`) REFERENCES `route_grade`(`route_grade_id`) ON UPDATE NO ACTION ON DELETE NO ACTION ," +
+                            "FOREIGN KEY(`location`) REFERENCES `location`(`location_id`) ON UPDATE NO ACTION ON DELETE NO ACTION );")
+                    database.execSQL("INSERT INTO attempt ( id,climb_style,outcome,route_grade,route_type,route_name,comment,rating,route_characteristics,length,location,instant,zone_id )" +
+                            "SELECT  id,climb_style,outcome,route_grade,route_type,route_name,comment,rating,routeCharacteristics,length,location,instant,zoneId " +
+                            "FROM attempt_old;")
+                    database.execSQL("DROP TABLE attempt_old;\n")
+                    database.execSQL("CREATE INDEX `index_attempt_location` ON `attempt` (`location`);")
+                    database.execSQL("CREATE INDEX `index_attempt_route_grade` ON `attempt` (`route_grade`);")
+                    database.execSQL("COMMIT;")
+                    database.execSQL("PRAGMA foreign_keys=on;")
+                }
+            }
+
             return Room.databaseBuilder(
                 context,
                 AppDatabase::class.java,
@@ -58,6 +90,7 @@ abstract class AppDatabase : RoomDatabase() {
                     }
                 })
                 .addMigrations(MIGRATION_2_3)
+                .addMigrations(MIGRATION_3_4)
                 .build()
         }
     }
